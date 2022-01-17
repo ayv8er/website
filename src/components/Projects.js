1645e85ca2cccf0c6dbcc3fe45a700d5b91decb1
Richard Oh <n24512@protonmail.com>
2022-01-17 11:27:41 -0600

    updates description for nasa project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,14 +11,15 @@ const Projects = () => {
     {
       title: "Cryptocurrency Index",
       description:
-        "A live market index of various Cryptocurrencies. A React application that manages two different sets of state using Redux; the main list which takes data via an Axios call from a CoinGecko API before setting it to state, and the other is a favorites list which manages a users selection from the main list. Dark mode is also set to state via a custom hook and local storage. A search input allows a user to filter the main list dynamically. The favorites list only appears after a user has set one item to the favorites list state. I plan on adding a login and register feature that gives a user the ability to use the favorites list feature, and persisting that data with a relational database.",
+        "A live market index of various Cryptocurrencies. A React SPA that manages two different sets of state using Redux; the main list which takes data via an Axios call from a CoinGecko API (https://www.coingecko.com/en/api) before setting it to state, and the other is a favorites list which manages a users selection from the main list. Dark mode is also set to state via a custom hook and local storage. A search input allows a user to filter the main list dynamically. The favorites list only appears after a user has set one item to the favorites list state. I plan on adding a login and register feature that gives a user the ability to use the favorites list feature, and persisting that data with a relational database.",
       gitHub: "https://github.com/ayv8er/cryptomarketinfo",
       deployed_site: "https://cryptomarketinfo.vercel.app/",
       card_image: cryptoImg,
     },
     {
       title: "NASA - APOD",
-      description: "Get a new photo from NASA every day.",
+      description:
+        'Get a new photo from NASA every day. A React SPA that consumes and displays a 3rd party API (https://api.nasa.gov/#apod) using an API key provided by NASA, to display an "Astronomy Photo of the Day" along with a title, date, copyright information, and description.',
       gitHub: "https://github.com/ayv8er/nasadailyphoto",
       deployed_site: "https://nasadailyphoto-phi.vercel.app/",
       card_image: nasaImg,
