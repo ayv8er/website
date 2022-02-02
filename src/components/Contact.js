bf67a11beddc0ce1fef39f25c9806e6003c6d107
Richard Oh <n24512@protonmail.com>
2022-02-02 09:32:19 -0600

    updates resume

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -13,7 +13,7 @@ const Contact = () => {
     linkedIn: "https://www.linkedin.com/in/rjo2/",
     gitHub: "https://github.com/ayv8er",
     resume:
-      "https://drive.google.com/file/d/1x4ue55nsDxrvl_omtK9hxaPQGdLdXgLN/view?usp=sharing",
+      "https://drive.google.com/file/d/1Xnz9116X4TwlgGbXWqdWlJKrqniMe255/view?usp=sharing",
   };
 
   return (
